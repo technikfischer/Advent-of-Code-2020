6ff5b1e6ed30b891c35cc5fd1139a95cce059786
Martin Peche <martin.peche@aon.at>
2020-12-10 19:13:50 +0100

    Day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,10 +1,12 @@
 use advent_of_code::get_input;
 use counter::Counter;
+use itertools::Itertools;
 
 fn main() {
     let mut adapters: Vec<usize> = get_input();
+    let device_joltage = adapters.iter().max().unwrap() + 3;
     adapters.push(0);
-    adapters.push(adapters.iter().max().unwrap() + 3);
+    adapters.push(device_joltage);
     adapters.sort_unstable();
 
     // part 1
@@ -14,5 +16,22 @@ fn main() {
         .collect::<Counter<_>>();
 
     println!("Joltage differences of 1 and 3 multiplied: {}", joltage_differences[&1] * joltage_differences[&3]);
+
+    let differences = adapters
+        .windows(2) // 3
+        .map(|w| w[1] - w[0])// w[2]
+        .collect_vec();
+
+    let arrangements = differences.split(|d| *d == 3).map(combinations).fold(1, |a, b| a * b);
+    println!("Possible arrangements {}", arrangements);
 }
 
+fn combinations(differences: &[usize]) -> i64 {
+    match differences {
+        [] => 1,
+        [3, ..] => combinations(&differences[1..]),
+        [1, 1, 1, ..] => combinations(&differences[1..]) + combinations(&differences[2..]) + combinations(&differences[3..]),
+        [1, 2, ..] | [2, 1, ..] | [1, 1, ..] => combinations(&differences[1..]) + combinations(&differences[2..]),
+        _ => combinations(&differences[1..])
+    }
+}
