f415483e67f037d8863dcc319f3c8b456ef2097e
Martin Peche <martin.peche@aon.at>
2020-12-11 13:44:48 +0100

    Day 11 part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -19,12 +19,20 @@ fn line_to_vector(line: &str) -> Vec<SeatType> {
     }).collect()
 }
 
-fn count_neighbours(seat_layout: &Vec<Vec<SeatType>>, r: usize, c: usize) -> Counts {
+fn get_seat_type(seat_layout: &SeatLayout, r: isize, c: isize) -> Option<SeatType> {
+    if r < 0 || c < 0 || r as usize >= seat_layout.len() || c as usize >= seat_layout[0].len() {
+        None
+    } else {
+        Some(seat_layout[r as usize][c as usize])
+    }
+}
+
+fn count_neighbours_adjacent(seat_layout: &SeatLayout, r: isize, c: isize) -> Counts {
     let mut counts: Counts = [0, 0, 0];
     for rd in -1..=1 {
         for cd in -1..=1 {
             if rd == 0 && cd == 0 { continue; }
-            if let Some(&seat_type) = seat_layout.get((r as isize + rd) as usize).and_then(|row| row.get((c as isize + cd) as usize)) {
+            if let Some(seat_type) = get_seat_type(&seat_layout, r + rd, c + cd) {
                 counts[seat_type] += 1;
             }
         }
@@ -32,17 +40,61 @@ fn count_neighbours(seat_layout: &Vec<Vec<SeatType>>, r: usize, c: usize) -> Cou
     counts
 }
 
-fn next_state(seat_layout: &SeatLayout) -> (SeatLayout, bool) {
+fn count_neighbours_visible(seat_layout: &SeatLayout, r: isize, c: isize) -> Counts {
+    let mut counts: Counts = [0, 0, 0];
+    for rd in -1..=1 {
+        for cd in -1..=1 {
+            if rd == 0 && cd == 0 { continue; }
+            let mut a = 1;
+            's: loop {
+                match get_seat_type(&seat_layout, r + a * rd, c + a * cd) {
+                    Some(st) if st == EMPTY || st == OCCUPIED => {
+                        counts[st] += 1;
+                        break 's;
+                    }
+                    Some(_) => {}
+                    None => { break 's; }
+                }
+                a += 1;
+            }
+        }
+    }
+    counts
+}
+
+fn next_state_part1(seat_layout: &SeatLayout) -> (SeatLayout, bool) {
     let mut changed = false;
     let new_state =
-        (0..seat_layout.len()).map(|row|
-            (0..seat_layout[0].len()).map(|col| {
-                let counts = count_neighbours(&seat_layout, row, col);
-                let seat = seat_layout[row][col];
+        (0isize..seat_layout.len() as isize).map(|row|
+            (0isize..seat_layout[0].len() as isize).map(|col| {
+                let counts = count_neighbours_adjacent(&seat_layout, row, col);
+                let seat = seat_layout[row as usize][col as usize];
                 if seat == EMPTY && counts[OCCUPIED] == 0 {
                     changed = true;
                     OCCUPIED
                 } else if seat == OCCUPIED && counts[OCCUPIED] >= 4 {
+                    changed = true;
+                    EMPTY
+                } else {
+                    seat
+                }
+            }).collect_vec()
+        ).collect_vec();
+
+    (new_state, changed)
+}
+
+fn next_state_part2(seat_layout: &SeatLayout) -> (SeatLayout, bool) {
+    let mut changed = false;
+    let new_state =
+        (0isize..seat_layout.len() as isize).map(|row|
+            (0isize..seat_layout[0].len() as isize).map(|col| {
+                let counts = count_neighbours_visible(&seat_layout, row, col);
+                let seat = seat_layout[row as usize][col as usize];
+                if seat == EMPTY && counts[OCCUPIED] == 0 {
+                    changed = true;
+                    OCCUPIED
+                } else if seat == OCCUPIED && counts[OCCUPIED] >= 5 {
                     changed = true;
                     EMPTY
                 } else {
@@ -78,7 +130,7 @@ fn main() {
     // part 1
     let mut new_seat_layout = seat_layout.clone();
     loop {
-        let (new_state, changed) = next_state(&new_seat_layout);
+        let (new_state, changed) = next_state_part1(&new_seat_layout);
         new_seat_layout = new_state;
         if !changed {
             break;
@@ -86,5 +138,18 @@ fn main() {
     }
 
     let occupied_seats: usize = new_seat_layout.iter().map(|row| row.iter().filter(|c| **c == OCCUPIED).count()).sum();
-    print!("Occupied seats {}", occupied_seats);
+    println!("Occupied seats part 1 {}", occupied_seats);
+
+    // part 2
+    let mut simulated = seat_layout.clone();
+    loop {
+        let (new_state, changed) = next_state_part2(&simulated);
+        simulated = new_state;
+        if !changed {
+            break;
+        }
+    }
+
+    let occupied_seats: usize = simulated.iter().map(|row| row.iter().filter(|c| **c == OCCUPIED).count()).sum();
+    println!("Occupied seats part 2 {}", occupied_seats);
 }
