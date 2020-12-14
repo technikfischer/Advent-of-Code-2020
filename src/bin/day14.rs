72e78fe229455ab00672747177e00fa6cb005a4f
Martin Peche <martin.peche@aon.at>
2020-12-14 17:27:01 +0100

    Day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 
 enum Instruction {
     SetMask(String),
-    WriteVal { addr: u64, val: String },
+    WriteVal { addr: String, val: String },
 }
 
 impl FromStr for Instruction {
@@ -17,18 +17,32 @@ impl FromStr for Instruction {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         lazy_static! {
-        static ref SET_MASK_PATTERN: Regex = Regex::new(r"mask = (?P<mask>[01X]{36})").unwrap();
-        static ref WRITE_MEMORY_PATTERN: Regex = Regex::new(r"mem\[(?P<addr>\d+)\] = (?P<value>\d+)").unwrap();
+            static ref SET_MASK_PATTERN: Regex = Regex::new(r"mask = (?P<mask>[01X]{36})").unwrap();
+            static ref WRITE_MEMORY_PATTERN: Regex = Regex::new(r"mem\[(?P<addr>\d+)\] = (?P<value>\d+)").unwrap();
         }
 
         if let Some(c) = SET_MASK_PATTERN.captures(s) {
             Ok(SetMask(c["mask"].to_owned()))
         } else if let Some(c) = WRITE_MEMORY_PATTERN.captures(s) {
-            let addr = c["addr"].parse().expect("Could not parse address of memory instruction");
-            let val: u64 = c["value"].parse().unwrap();
-            Ok(WriteVal { addr, val: format!("{:036b}", val) })
+            let addr: u64 = c["addr"].parse().expect("Could not parse address of memory instruction");
+            let val: u64 = c["value"].parse().expect("Could not parse value of memory instruction");
+            Ok(WriteVal { addr: format!("{:036b}", addr), val: format!("{:036b}", val) })
         } else {
             Err(s.to_owned())
+        }
+    }
+}
+
+fn write_val(memory: &mut HashMap<String, String>, pos: isize, addr: String, val: &String) {
+    if pos < 0 {
+        memory.insert(addr, val.to_owned());
+        return;
+    } else {
+        if addr.chars().nth(35 - pos as usize).unwrap() != 'X' {
+            write_val(memory, pos - 1, addr, val);
+        } else {
+            write_val(memory, pos - 1,  addr.replacen('X', "0", 1), val);
+            write_val(memory, pos - 1,  addr.replacen('X', "1", 1), val);
         }
     }
 }
@@ -38,26 +52,44 @@ fn main() {
 
     // part 1
     let mut mask = String::new();
-    let mut memory: HashMap<u64, String> = HashMap::new();
+    let mut memory: HashMap<String, String> = HashMap::new();
 
-    for inst in instructions {
+    for inst in instructions.iter() {
         match inst {
-            SetMask(m) => mask = m,
+            SetMask(m) => mask = m.to_string(),
             Instruction::WriteVal { addr, val } => {
                 let newval = mask.chars()
                     .zip(val.chars())
                     .map(|(m, v)| if m == 'X' { v } else { m })
                     .collect::<String>();
 
-                if u64::from_str_radix(&newval, 2).unwrap() == 0 {
-                    memory.remove(&addr);
-                } else {
-                    memory.insert(addr, newval);
-                }
+                memory.insert(addr.to_string(), newval);
             }
         }
     }
 
     let sum: u64 = memory.values().map(|s| u64::from_str_radix(s, 2).unwrap()).sum();
-    println!("Sum of all non-zero memory cells is {}", sum);
+    println!("Sum of all non-zero memory cells (part 1) is {}", sum);
+
+    // part 2
+    let mut mask = String::new();
+    let mut memory: HashMap<String, String> = HashMap::new();
+
+    for inst in instructions {
+        match inst {
+            SetMask(m) => mask = m,
+            Instruction::WriteVal { addr, val } => {
+                let mask_applied = mask.chars()
+                    .zip(addr.chars())
+                    .map(|(m, v)| if m == '0' { v } else { m })
+                    .collect::<String>();
+
+                //println!("addr: {} val: {} cur mask: {} mask @ addr: {}", addr, val, mask, mask_applied);
+                write_val(&mut memory, 35, mask_applied, &val);
+            }
+        }
+    }
+
+    let sum: u64 = memory.values().map(|s| u64::from_str_radix(s, 2).unwrap()).sum();
+    println!("Sum of all non-zero memory cells (part2) is {}", sum);
 }
