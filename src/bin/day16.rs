9117cda6778abe4312a199b2a3502c8c66183457
Martin Peche <martin.peche@aon.at>
2020-12-16 14:50:18 +0100

    Day 16 part 2 finished

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,40 +1,47 @@
 use std::fs;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use pom::parser::*;
 use pom::char_class::{multispace, alpha};
+use itertools::Itertools;
+use bimap::BiMap;
 
-type Ticket = Vec<u32>;
+type Ticket = Vec<u64>;
 
 
 #[derive(Debug)]
 struct Input {
-    rules: HashMap<String, [(u32, u32); 2]>,
+    rules: Vec<HashSet<u64>>,
     my_ticket: Ticket,
     nearby_tickets: Vec<Ticket>,
 }
 
-fn integer<'a>() -> Parser<'a, u8, u32> {
-    one_of(b"0123456789").repeat(1..).convert(String::from_utf8).convert(|s| s.parse::<u32>())
+fn integer<'a>() -> Parser<'a, u8, u64> {
+    one_of(b"0123456789").repeat(1..).convert(String::from_utf8).convert(|s| s.parse::<u64>())
 }
 
 fn space<'a>() -> Parser<'a, u8, ()> {
     is_a(multispace).repeat(1..).discard()
 }
 
-fn range<'a>() -> Parser<'a, u8, (u32, u32)> {
+fn range<'a>() -> Parser<'a, u8, (u64, u64)> {
     integer() - sym(b'-') + integer()
 }
 
-fn rule<'a>() -> Parser<'a, u8, (String, [(u32, u32); 2])> {
+fn rule<'a>() -> Parser<'a, u8, (String, HashSet<u64>)> {
     let rule_name = (is_a(alpha) | is_a(pom::char_class::space)).repeat(1..).convert(String::from_utf8);
+    let range = || range().map(|(start, end)| (start..=end).collect::<HashSet<_>>());
     let ranges = range() - seq(b" or ") + range();
 
     let rule = rule_name - sym(b':') - space() + ranges;
-    rule.map(|(name, (range0, range1))| (name, [range0, range1]))
+    rule.map(|(name, (range0, range1))| (name, range0.union(&range1).copied().collect()))
 }
 
 fn ticket<'a>() -> Parser<'a, u8, Ticket> {
-    list(integer(), sym(b','))
+    let least_one = integer() - sym(b',') + list(integer(), sym(b','));
+    least_one.map(|(first, mut rest)| {
+        rest.insert(0, first);
+        rest
+    })
 }
 
 fn input<'a>() -> Parser<'a, u8, Input> {
@@ -45,10 +52,46 @@ fn input<'a>() -> Parser<'a, u8, Input> {
     let input = rules - space().opt() + my_ticket - space().opt() + nearby_tickets - space().opt() - end();
 
     input.map(|((rules, my_ticket), nearby_tickets)| Input {
-        rules: rules.into_iter().collect(),
+        rules: rules.into_iter().map(|(_, valid)| valid).collect(),
         my_ticket,
         nearby_tickets,
     })
+}
+
+fn does_rule_fit_col(rules: &Vec<HashSet<u64>>, tickets: &Vec<Ticket>, rule: usize, target_col: usize) -> bool {
+    tickets.iter().enumerate().all(|t| rules[rule].contains(&t.1[target_col]))
+}
+
+fn test_loose_rules(rules: &Vec<HashSet<u64>>, tickets: &Vec<Ticket>, matched_rules_to_col: &mut BiMap<usize, usize>) {
+    // test for each rule if it fits to a column, and if it is the only option
+    'outer: for rule_index in 0..20 { // just hardcoded value to speed up development
+        if matched_rules_to_col.contains_left(&rule_index) {
+            // skip rules which are already matched
+            continue;
+        }
+        let mut matched_col = None;
+        // find all matching columns, exclude search in columns who already got a rule matched
+        for col_index in 0..20 { // just hardcoded value to speed up development
+            // skips cols already matched
+            if matched_rules_to_col.contains_right(&col_index) {
+                continue;
+            }
+            if does_rule_fit_col(&rules, &tickets, rule_index, col_index) {
+                match matched_col {
+                    Some(_) => { continue 'outer; }
+                    None => { matched_col = Some(col_index); }
+                }
+            }
+        }
+
+        // when the loop was not terminated, there should be one column found
+        match matched_col {
+            Some(col) => {
+                matched_rules_to_col.insert(rule_index, col);
+            }
+            None => panic!("No col found, there should be one though")
+        }
+    }
 }
 
 fn main() {
@@ -57,19 +100,32 @@ fn main() {
 
     // part 1
     let mut valid_numbers = HashSet::new();
-    for (_, &ranges) in input.rules.iter() {
-        for range in 0..ranges.len() {
-            for number in ranges[range].0..=ranges[range].1 {
-                valid_numbers.insert(number);
-            }
-        }
+    for valid in input.rules.iter() {
+        valid_numbers = valid_numbers.union(valid).copied().collect();
     }
 
-    let error_rate : u32 = input.nearby_tickets.iter()
+    let error_rate: u64 = input.nearby_tickets.iter()
         .flat_map(|ticket| ticket.iter())
         .filter(|number| !valid_numbers.contains(number))
         .copied()
         .sum();
 
     println!("Ticket Scanning Error Rate is {}", error_rate);
+    let mut nearby_tickets = input.nearby_tickets.into_iter()
+        .filter(|ticket| ticket.iter().all(|n| valid_numbers.contains(n)))
+        .collect_vec();
+
+    nearby_tickets.insert(0, input.my_ticket);
+    let mut rules_to_cols = BiMap::new();
+    for _ in 0..20 {
+        test_loose_rules(&input.rules, &nearby_tickets, &mut rules_to_cols);
+        println!("{:?}", rules_to_cols);
+    }
+
+    let mut product = 1;
+    for rule in 0..6 { // those starting with departure
+        product *= nearby_tickets[0][*rules_to_cols.get_by_left(&rule).unwrap()];
+    }
+
+    println!("Product of all departure fields is {}", product);
 }
