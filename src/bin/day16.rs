070d05245e07098a4832a11f8f9b1ac61346f9ea
Martin Peche <martin.peche@aon.at>
2020-12-16 13:18:01 +0100

    Day 16, starting implementation for parser

---
+++ src/bin/day16.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use std::collections::{HashMap};
+use std::ops::Range;
+use pom::parser::*;
+use pom::char_class::{multispace, alpha};
+
+type Ticket = Vec<u32>;
+
+
+#[derive(Debug)]
+struct Input {
+    rules: HashMap<String, [Range<u32>; 2]>,
+    my_ticket: Ticket,
+    nearby_tickets: Vec<Ticket>,
+}
+
+fn integer<'a>() -> Parser<'a, u8, u32> {
+    one_of(b"0123456789").repeat(1..).convert(String::from_utf8).convert(|s| s.parse::<u32>())
+}
+
+fn space<'a>() -> Parser<'a, u8, ()> {
+    is_a(multispace).repeat(1..).discard()
+}
+
+fn range<'a>() -> Parser<'a, u8, Range<u32>> {
+    (integer() - space() - sym(b'-') - space() + integer()).map(|(start, end)| start..end + 1)
+}
+
+fn rule<'a>() -> Parser<'a, u8, (String, [Range<u32>; 2])> {
+    let rule_name = (is_a(alpha) | is_a(pom::char_class::space)).repeat(1..).convert(String::from_utf8);
+    let ranges = range() - space() - seq(b"or") - space() + range();
+    let rule = rule_name - space() - sym(b':') - space() + ranges;
+    rule.map(|(name, (range0, range1))| (name, [range0, range1]))
+}
+
+fn ticket<'a>() -> Parser<'a, u8, Ticket> {
+    list(integer(), sym(b','))
+}
+
+fn input<'a>() -> Parser<'a, u8, Input> {
+    let rules = list(rule(), space());
+    let my_ticket = seq(b"your ticket:") * space() * ticket();
+    let nearby_tickets = seq(b"nearby ticket:\n") * list(ticket(), space());
+
+    let input = rules + my_ticket + nearby_tickets;
+
+    input.map(|((rules, my_ticket), nearby_tickets)| Input {
+        rules: rules.into_iter().collect(),
+        my_ticket,
+        nearby_tickets,
+    })
+}
+
+fn main() {
+    let file = fs::read_to_string("input").expect("Could not open file");
+    let input = input().parse(file.as_bytes());
+    println!("{:?}", input);
+}
