d9aeaabe040d8034b694c7240f6499a45aa81611
Martin Peche <martin.peche@aon.at>
2020-12-18 10:52:09 +0100

    Day 18 part 2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 
 
 #[derive(Debug)]
-enum Expr<T> {
+pub enum Expr<T> {
     Add(Box<Expr<T>>, Box<Expr<T>>),
     Mul(Box<Expr<T>>, Box<Expr<T>>),
     Constant(T),
@@ -31,25 +31,61 @@ fn space<'a>() -> Parser<'a, u8, ()> {
     sym(b' ').repeat(0..).discard()
 }
 
-fn value<'a>() -> Parser<'a, u8, Expr<u64>> {
-    integer() | parentheses()
+mod part1 {
+    use pom::parser::*;
+    use crate::*;
+
+    fn value<'a>() -> Parser<'a, u8, Expr<u64>> {
+        integer() | parentheses()
+    }
+
+    fn parentheses<'a>() -> Parser<'a, u8, Expr<u64>> {
+        sym(b'(') * space() * call(expression) - space() - sym(b')')
+    }
+
+    pub fn expression<'a>() -> Parser<'a, u8, Expr<u64>> {
+        let right_operand = space() * one_of(b"+*") - space() + value();
+        let expr = value() + right_operand.repeat(0..);
+        expr.map(|(left, right)|
+            right.into_iter().fold(left, |curr, (op, r)|
+                match op {
+                    b'+' => Expr::create_add((curr, r)),
+                    b'*' => Expr::create_mul((curr, r)),
+                    _ => panic!("Invalid operator")
+                },
+            ))
+    }
 }
 
-fn parentheses<'a>() -> Parser<'a, u8, Expr<u64>> {
-    sym(b'(') * space() * call(expression) - space() - sym(b')')
-}
+mod part2 {
+    use pom::parser::*;
+    use crate::*;
 
-fn expression<'a>() -> Parser<'a, u8, Expr<u64>> {
-    let right_operand = space() * one_of(b"+*") - space() + value();
-    let expr = value() + right_operand.repeat(0..);
-    expr.map(|(left, right)|
-        right.into_iter().fold(left, |curr, (op, r)|
-            match op {
-                b'+' => Expr::create_add((curr, r)),
-                b'*' => Expr::create_mul((curr, r)),
-                _ => panic!("Invalid operator")
-            },
-        ))
+    fn factor<'a>() -> Parser<'a, u8, Expr<u64>> {
+        integer() | parentheses()
+    }
+
+    fn parentheses<'a>() -> Parser<'a, u8, Expr<u64>> {
+        sym(b'(') * space() * call(expression) - space() - sym(b')')
+    }
+
+    fn term<'a>() -> Parser<'a, u8, Expr<u64>> {
+        let right_operand = space() * sym(b'+') * space() * factor();
+        let expr = factor() + right_operand.repeat(0..);
+        expr.map(|(left, right)|
+            right.into_iter().fold(left, |curr, right|
+                Expr::create_add((curr, right)),
+            ))
+    }
+
+    pub fn expression<'a>() -> Parser<'a, u8, Expr<u64>> {
+        let right_operand = space() * sym(b'*') * space() * term();
+        let expr = term() + right_operand.repeat(0..);
+        expr.map(|(left, right)|
+            right.into_iter().fold(left, |curr, right|
+                Expr::create_mul((curr, right)),
+            ))
+    }
 }
 
 fn eval_expression(expr: &Expr<u64>) -> u64 {
@@ -66,10 +102,21 @@ fn eval_expression(expr: &Expr<u64>) -> u64 {
 
 fn main() {
     let file = fs::read_to_string("input").expect("Could not open file");
-    let expressions : u64 = file.lines()
-        .map(|line| expression().parse(line.as_bytes()).unwrap())
+    let expressions = file.lines().collect_vec();
+
+    // part 1
+    let expr_sum: u64 = expressions.iter()
+        .map(|line| part1::expression().parse(line.as_bytes()).unwrap())
         .map(|e| eval_expression(&e))
         .sum();
 
-    println!("Sum of all expressions is {}", expressions);
+    println!("Sum of all expressions is {}", expr_sum);
+
+    // part 2
+    let expr_sum: u64 = expressions.iter()
+        .map(|line| part2::expression().parse(line.as_bytes()).unwrap())
+        .map(|e| eval_expression(&e))
+        .sum();
+
+    println!("Sum of all expressions is {}", expr_sum);
 }
