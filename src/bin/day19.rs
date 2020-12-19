ded63b429e2750a1acea8f0fe9ce629dd6af891a
Martin Peche <martin.peche@aon.at>
2020-12-19 14:00:58 +0100

    Backtracking Pfusch

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -32,13 +32,16 @@ fn parse_rule(rule: &str) -> Rule {
     }
 }
 
-fn does_match<'a>(string: &'a str, pattern: &Rule, rules: &Vec<Rule>) -> (bool, &'a str) {
+fn does_match<'a>(string: &'a str, pattern: &Rule, rules: &Vec<Rule>, last_branch: bool) -> (bool, &'a str) {
+    if string.len() == 0 {
+        return (false, string);
+    }
     match pattern {
-        Rule::String(p) => (string.starts_with(p), &string[p.len()..]),
+        Rule::String(p) => (string.starts_with(p) && (!last_branch || p.len() == string.len()), &string[p.len()..]),
         Rule::Sequence(seq) => {
             let mut string = string;
-            for rule in seq.iter().map(|&id| &rules[id]) {
-                let (matches, rest) = does_match(string, rule, rules);
+            for (idx, rule) in seq.iter().map(|&id| &rules[id]).enumerate() {
+                let (matches, rest) = does_match(string, rule, rules, last_branch && (idx == seq.len() - 1));
                 if matches {
                     string = rest;
                 } else {
@@ -48,25 +51,29 @@ fn does_match<'a>(string: &'a str, pattern: &Rule, rules: &Vec<Rule>) -> (bool, 
             (true, string)
         }
         Rule::Alternation(left, right) => {
-            let (left_match, rest) = does_match(string, left, rules);
+            let (left_match, rest) = does_match(string, left, rules, false);
             if left_match {
                 return (true, rest);
             }
-            does_match(string, right, rules)
+            does_match(string, right, rules, last_branch)
         }
     }
 }
 
 fn main() {
     let rules = fs::read_to_string("rules").expect("Could not open file");
-    let rules = rules.lines().sorted_by_key(|e| e.split(':').nth(0).map(|s| s.parse::<u32>().unwrap())).map(parse_rule).collect _vec();
+    let mut rules = rules.lines().sorted_by_key(|e| e.split(':').nth(0).map(|s| s.parse::<u32>().unwrap())).map(parse_rule).collect_vec();
+
+    // replace rules
+    rules[8] = Rule::Alternation(Box::new(Rule::Sequence(vec![42])), Box::new(Rule::Sequence(vec![42, 8])));
+    rules[11] = Rule::Alternation(Box::new(Rule::Sequence(vec![42, 31])), Box::new(Rule::Sequence(vec![42, 11, 31])));
 
     let input = fs::read_to_string("input").expect("Could not open file");
     let input = input.lines().collect_vec();
 
     let mut amount = 0;
     for i in input {
-        if let (true, "") = does_match(i, &rules[0], &rules) {
+        if let (true, "") = does_match(i, &rules[0], &rules, true) {
             amount += 1;
         }
     }
